ecb8c2c8ff14c429cf5f8fa343d5605b70607624
MichaelLee826 <michaelleef2008@gmail.com>
2020-07-28 15:46:34 +0800

    基于sklearn，用决策树实现分类

---
+++ iris_data_decision_tree_sklearn.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+
+iris_data_set = pd.read_csv('E:\\PyCharm-Workspace\\DataAnalysis\\data\\03 Iris\\DecisionTreeClassifier\\iris.csv')
+
+# x是4列特征
+x = iris_data_set.iloc[:, 0:4].values
+# y是1列标签
+y = iris_data_set.iloc[:, -1].values
+
+# 划分训练集和测试集
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+# 利用GridSearchCV选择最优参数
+param = {'criterion': ['gini', 'entropy'], 'max_depth': [30, 50, 60, 100], 'min_samples_leaf': [2, 3, 5, 10], 'min_impurity_decrease': [0.1, 0.2, 0.5]}
+grid = GridSearchCV(DecisionTreeClassifier(), param_grid=param, cv=5)
+grid.fit(x_train, y_train)
+print('最优分类器:', grid.best_params_, '最优分数:', grid.best_score_)  # 得到最优的参数和分值
+
+# 利用决策树分类器构建分类模型
+model = DecisionTreeClassifier(criterion='entropy', max_depth=30, min_samples_leaf=2, min_impurity_decrease=0.1)
+model.fit(x_train, y_train)
+y_pre = model.predict(x_test)
+
+print('正确标签：', y_test)
+print('预测结果：', y_pre)
+
+print('训练集分数：', model.score(x_train, y_train), '测试集分数：', model.score(x_test, y_test))
+
+# 混淆矩阵
+conf_mat = confusion_matrix(y_test, y_pre)
+print('混淆矩阵：')
+print(conf_mat)
+
+# 分类指标文本报告（精确率、召回率、F1值等）
+print('分类指标报告：')
+print(classification_report(y_test, y_pre))
+
+print(model.feature_importances_)
+
+# 画图展示训练结果
+fig = plt.figure()
+ax = fig.add_subplot(111)
+f1 = ax.scatter(list(range(len(x_test))), y_test, marker='*')
+f2 = ax.scatter(list(range(len(x_test))), y_pre, marker='o')
+plt.legend(handles=[f1, f2], labels=['True', 'Prediction'])
+plt.show()
